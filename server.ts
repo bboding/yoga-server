c074dc3a569d2ce012239dc3e192391df8004f11
bboding <dingbboding@gmail.com>
2021-10-28 21:12:47 +0900

    첫 커밋

---
+++ server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 5000
+
+class App {
+  public application: express.Application
+  constructor() {
+    this.application = express()
+  }
+}
+
+const app = new App().application
+
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  console.log('///a')
+  res.send('server testing ok')
+})
+
+app.get('/data/1', (req: Request, res: Response) => {
+  res.json({ id: 1, name: 'pepper' })
+})
+
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
