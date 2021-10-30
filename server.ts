d085656955fa138226fa53f9f089bd79180b6b3a
bboding <dingbboding@gmail.com>
2021-10-30 11:56:31 +0900

    pose 조회

--- server.ts
+++ server.ts
@@ -1,5 +1,26 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+const { Client } = require('pg')
+const Query = require('pg').Query
+
+const client = new Client({
+  user: 'uohjxbyrzzdezi',
+  host: 'ec2-67-202-36-228.compute-1.amazonaws.com',
+  database: 'd8nokjbf74nhet',
+  password: 'f6a7af5ce3488a5369b722ae3d9a5e3b22667a319c1cfd1912466c2212b22fa9',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+client.connect((err: any) => {
+  if (err) {
+    console.error('error ocurred', err)
+  } else {
+    console.log('succeed')
+  }
+})
 
 const PORT = process.env.PORT || 3000
 
@@ -15,12 +36,25 @@ const app = new App().application
 app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
-  console.log('///a')
   res.send('server testing ok')
 })
 
-app.get('/data/1', (req: Request, res: Response) => {
-  res.json({ id: 1, name: 'pepper' })
+
+app.get('/poses', (req: Request, res: Response) => {
+  client.query('select * from poses where visible = true order by ordering desc', (error: any, results: { rows: any }) => {
+    if (error) throw error
+
+    res.json(results.rows)
+  })
+
+})
+
+app.get('/poses/:id', (req: Request, res: Response) => {
+  client.query(`select * from poses where visible = true and id = ${req.params.id} order by ordering desc`, (error: any, results: { rows: any }) => {
+    if (error) throw error
+
+    res.json(results.rows)
+  })
 })
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
